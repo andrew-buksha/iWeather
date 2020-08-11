3652f54c1418f1d107f8460d4add667642e91e90
Rodion Stukalo <d_rstukalo@universalweather.net>
2020-08-11 20:22:17 +0300

    initial commit

---
+++ iWeather/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  iWeather
+//
+//  Created by Andrey Buksha on 11.08.2020.
+//  Copyright © 2020 Andrey Buksha. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ iWeather/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  iWeather
+//
+//  Created by Andrey Buksha on 11.08.2020.
+//  Copyright © 2020 Andrey Buksha. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
